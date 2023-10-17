8dc87c199289ab22ae531f53eb48ddcb763b6db9
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2023-10-17 21:12:57 +0100

    FInals

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 # Group L Team Task 
 
 # Welcome!Group Members
-/n Group Members
 
-
-
-<!-- ************ STEPS TO FOLLOW TO PUSH TO OUR REPO **************** -->
-git add .
-
-git commit -m "Added styling"
-
-git push origin main
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const phoneInput = document.querySelector(".phone");
 const providerLogo = document.querySelector(".provider-logo");
+const errormsg = document.querySelector('.error');
+
 
 function detectProvider(phoneNumber) {
   const mtnPattern = /^(\+234|0)(703|706|803|806|810|813|814|816|903|906|913)/;
@@ -8,6 +10,7 @@ function detectProvider(phoneNumber) {
   const etisalatPattern = /^(\+234|0)(809|817|818|908|909|807)/;
 
   providerLogo.classList.remove("mtn", "glo", "airtel", "nineMobile");
+  errormsg.classList.remove("errormsg");
 
   if (mtnPattern.test(phoneNumber)) {
     providerLogo.classList.add("mtn");
@@ -17,11 +20,18 @@ function detectProvider(phoneNumber) {
     providerLogo.classList.add("airtel");
   } else if (etisalatPattern.test(phoneNumber)) {
     providerLogo.classList.add("nineMobile");
+  } 
+  else {
+    errormsg.classList.add("errormsg");
   }
 }
+
 
 // Event listener to detect changes in the input field
 phoneInput.addEventListener("input", function () {
   const phoneNumber = this.value;
   detectProvider(phoneNumber);
+  if (phoneInput.value === '' ){
+    errormsg.classList.remove('errormsg')
+  }
 });
